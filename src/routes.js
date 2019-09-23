183188ac8a1bb5665400148c4cbaaeffae1b76a5
Juan Felix <asxy.2015@gmail.com>
2019-09-22 21:55:35 -0300

    bug fix 5.0

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/login" exact={true} component={Login}/>
+      <Route path="/" exact={true} component={Login}/>
       <Route path="/regulations" exact={true} component={EventInfo}/>
       <Route path="/main" exact={true} component={Main}/>
       <Route path="/user/:id" exact={true} component={User}/>
