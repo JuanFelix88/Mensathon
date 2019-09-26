881f880149e5bc38ad795d18f7b5527674c60c80
Juan Felix <asxy.2015@gmail.com>
2019-09-26 17:13:26 -0300

    Change directory of component team

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import PageNotFound from './pages/404'
 import Login from './pages/login'
 import EventInfo from './pages/regulations'
-import Main from './pages/main'
+import Team from './pages/team'
 import User from './pages/user'
 import Register from './pages/register'
 // import FindPeople from './pages/findpeople'
@@ -15,7 +15,7 @@ const Routes = () => (
       <Route path="/register" exact={true} component={Register}/>
       <Route path="/" exact={true} component={Login}/>
       <Route path="/regulations" exact={true} component={EventInfo}/>
-      <Route path="/main" exact={true} component={Main}/>
+      <Route path="/team" exact={true} component={Team}/>
       <Route path="/user/:id" exact={true} component={User}/>
       {/* <Route path="/findpeople" exact={true} component={FindPeople}/> */}
       {/* findpeople */}
